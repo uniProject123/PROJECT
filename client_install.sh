b7fb72800def02086c1c424158f2e859f5bc3087
uniProject123 <97971558+uniProject123@users.noreply.github.com>
2024-05-14 18:07:22 +0100

    client_install.sh

--- client_install.sh
+++ client_install.sh
@@ -6,6 +6,10 @@ sudo apt update
 # Install Python3 and pip
 sudo apt install -y python3 python3-pip python3.12-venv
 
+# Create and Source the Environment
+python3 -m venv .venv
+source .venv/bin/activate
+
 # Install PyTorch and torchvision (assuming ARM architecture)
 sudo pip3 install torch torchvision
 
